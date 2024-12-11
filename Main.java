201f283aae10d40b18dc0271476817079580a893
PinguinLars1105 <Heijmen.Lars@gmail.com>
2024-12-11 13:40:32 +0100

    Imported a ScreenShotter.java into Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,2 @@
 package main;
-import java.io.File;
-import java.io.FileWriter;
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
+import Recources.ScreenShotter;
