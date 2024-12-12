80f49b02661e3ca1fdc60289afaadcb4b2cfdb8c
PinguinLars1105 <Heijmen.Lars@gmail.com>
2024-12-12 13:44:08 +0100

    Final changes of today

--- Recources/ColorDetector.java
+++ Recources/ColorDetector.java
@@ -6,28 +6,24 @@ import java.awt.Color;
 public class ColorDetector {
     public static void main(String[] args) {
         try {
-            int x = 50;
-            int y = 100;
-            int placeholder = 255;
-
-            while (x < 100) {
+            int x = 263;
+            int y = 69;
+            while (x < 877) {
                 File file = new File("screenshot.png");
                 BufferedImage image = ImageIO.read(file);
                 int pixelColor = image.getRGB(x, y);
                 Color color = new Color(pixelColor, true);
-                if (color.getRed() < placeholder) {
-                    if (color.getGreen() < placeholder) {
-                        if (color.getBlue() < placeholder) {
-                            if (color.getAlpha() < placeholder) {
+                if (color.getRed() < 125) {
+                    if (color.getGreen() < 125) {
+                        if (color.getBlue() < 125) {
                                 System.out.println("ColorDetector: Black detected");
-                            }
                         }
                     }
                 }
                 y += 1;
-                if (y > 110) {
-                    y = 100;
-                    x++;
+                if (y < 682) {
+                    y = 69;
+                    x += 1;
                 }
             }
         } catch (Exception e) {
