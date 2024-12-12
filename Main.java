6241f0c857a78a713df3a791e30ba41bb241fb99
PinguinLars1105 <Heijmen.Lars@gmail.com>
2024-12-12 11:02:05 +0100

    Screenshotter can now run from Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,10 @@
-package main;
 import Recources.ScreenShotter;
+public class Main {
+    public static void main(String[] args) {
+        try {
+            ScreenShotter.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
