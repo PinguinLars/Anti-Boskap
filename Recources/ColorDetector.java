93f0c512590c79cd763b05f2e68e64c9927aa862
PinguinLars1105 <Heijmen.Lars@gmail.com>
2024-12-12 13:14:36 +0100

    Made placeholders clearer

--- Recources/ColorDetector.java
+++ Recources/ColorDetector.java
@@ -8,26 +8,22 @@ public class ColorDetector {
         try {
             int x = 50;
             int y = 100;
+            int placeholder = 255;
 
             while (x < 100) {
                 File file = new File("screenshot.png");
                 BufferedImage image = ImageIO.read(file);
                 int pixelColor = image.getRGB(x, y);
                 Color color = new Color(pixelColor, true);
-                // 255 is a placeholder.
-                if (color.getRed() < 255) {
-                    if (color.getGreen() < 255) {
-                        if (color.getBlue() < 255) {
-                            if (color.getAlpha() < 255) {
+                if (color.getRed() < placeholder) {
+                    if (color.getGreen() < placeholder) {
+                        if (color.getBlue() < placeholder) {
+                            if (color.getAlpha() < placeholder) {
                                 System.out.println("ColorDetector: Black detected");
                             }
                         }
                     }
                 }
-//                System.out.println("Red: " + color.getRed());
-//                System.out.println("Green: " + color.getGreen());
-//                System.out.println("Blue: " + color.getBlue());
-//                System.out.println("Alpha: " + color.getAlpha());
                 y += 1;
                 if (y > 110) {
                     y = 100;
