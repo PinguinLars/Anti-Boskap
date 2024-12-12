06eb625ad55c30ef1a7041e4e00a69d5c4f57e03
PinguinLars1105 <Heijmen.Lars@gmail.com>
2024-12-12 12:22:22 +0100

    Setup base for color dection

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
-import Recources.ScreenShotter;
+import Recources.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 public class Main {
     public static void main(String[] args) {
         try {
             ScreenShotter.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void second(String[] args) {
+        try {
+            ColorDector.main(args);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- Recources/ColorDector.java
+++ Recources/ColorDector.java
@@ -3,3 +3,8 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+public class ColorDector{
+    public static void main(String[] args) throws Exception {
+
+    }
+}
