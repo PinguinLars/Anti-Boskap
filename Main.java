6703198a07505b6f95ad36dad18aadd0c454eeec
PinguinLars1105 <Heijmen.Lars@gmail.com>
2024-12-16 08:56:30 +0100

    Trying to make the webhook task run in the background

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import Recources.*;
+import java.io.IOException;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             //Runs ColorDetector and ScreenShotter.
             ScreenShotter.main(args);
@@ -10,8 +11,10 @@ public class Main {
         }
     System.out.println("Black Detected: (1 is true, 0 is false)");
     System.out.println(ColorDetector.black);
-        //Send a message to WIP webhook
+        //Send a message to WIP webhook in the background
 //        if (ColorDetector.black > 0) {
+//            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "start", "java", "TeamsLogger");
+//            pb.start();
 ////            TeamsLogger.WebhookMessage = "ALARM";
 //            TeamsLogger.WebhookMessage = "Black Detected";
 //            TeamsLogger.main(args);
--- Recources/ColorDetector.java
+++ Recources/ColorDetector.java
@@ -6,22 +6,24 @@ import java.awt.Color;
 public class ColorDetector {
     public static int black = 0;
     public static void main(String[] args) {
-        try {
-            File file = new File("screenshot.png");
-            BufferedImage image = ImageIO.read(file);
-            for (int x = 263; x < 877; x++) {
-                for (int y = 69; y < 682; y++) {
-                    int pixelColor = image.getRGB(x,y);
-                    Color color = new Color(pixelColor, true);
-                    if (color.getRed() < 125 && color.getGreen() < 125 && color.getBlue() < 125) {
-                        System.out.println("ColorDetector: Black detected");
-                        black = 1;
+//        while (true) {
+            try {
+                File file = new File("screenshot.png");
+                BufferedImage image = ImageIO.read(file);
+                for (int x = 263; x < 877; x++) {
+                    for (int y = 69; y < 682; y++) {
+                        int pixelColor = image.getRGB(x, y);
+                        Color color = new Color(pixelColor, true);
+                        if (color.getRed() < 125 && color.getGreen() < 125 && color.getBlue() < 125) {
+                            System.out.println("ColorDetector: Black detected");
+                            black = 1;
+                        }
                     }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println("ColorDetector: Successful execution");
+            System.out.println("ColorDetector: Successful execution");
+        //}
     }
 }
