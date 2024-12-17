8d3a96d3a89d4acf9ff922254f617184f57d0094
PinguinLars1105 <Heijmen.Lars@gmail.com>
2024-12-17 12:27:06 +0100

    Made it so you can run it in a loop for the end of time.

--- Main.java
+++ Main.java
@@ -1,19 +1,21 @@
 import Recources.*;
 import java.io.IOException;
 public class Main {
-    public static void main(String[] args) throws IOException {
-        try {
-            //Voert ColorDetector and ScreenShotter uit.
-            ScreenShotter.main(args);
-            ColorDetector.main(args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    System.out.println("Zwart Gedecteerd: (1 is ja, 0 is nee)");
-    System.out.println(ColorDetector.black);
-        //Stuur in bericht naar de webhook in de achtergrond.
-        if (ColorDetector.black > 0) {
-            TeamsLogger.main(args);
+    public static void main (String[]args) throws IOException {
+//        while (true) {
+            try {
+                //Voert ColorDetector and ScreenShotter uit.
+                ScreenShotter.main(args);
+                ColorDetector.main(args);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Zwart Gedecteerd: (1 is ja, 0 is nee)");
+            System.out.println(ColorDetector.black);
+            //Stuur in bericht naar de webhook in de achtergrond.
+            if (ColorDetector.black > 0) {
+                TeamsLogger.main(args);
+//            }
         }
     }
 }
