c8184df4bd583844e7629e4820962377b46e3192
PinguinLars1105 <68437296+PinguinLars@users.noreply.github.com>
2024-12-21 10:30:30 +0100

    Removed not needed code
    Signed-off-by: PinguinLars1105 <68437296+PinguinLars@users.noreply.github.com>

--- Recources/MicroMacro.py
+++ Recources/MicroMacro.py
@@ -1,10 +1,7 @@
 import pyautogui
-import time
 # Kan zijn dat het programma nog aan het downloaden is terwijl dit script al klaar is.
-# time.sleep(1)
 pyautogui.hotkey('alt', 'tab')
 pyautogui.moveTo(279, 703)
 pyautogui.click()
-# time.sleep(3)
 pyautogui.moveTo(11511, 511)
 pyautogui.hotkey('alt', 'tab')
