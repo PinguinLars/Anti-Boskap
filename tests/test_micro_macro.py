c433ea71caff973ad6969ae84dde98121437ffb7
Lars <68437296+PinguinLars@users.noreply.github.com>
2025-01-14 13:25:00 +0100

    Update test_micro_macro.py

--- tests/test_micro_macro.py
+++ tests/test_micro_macro.py
@@ -1,7 +1,11 @@
-import subprocess
 import os
+import subprocess
+import pytest
+
+@pytest.fixture(autouse=True)
+def set_display_env(monkeypatch):
+    monkeypatch.setenv('DISPLAY', ':0')
 
 def test_micro_macro():
     result = subprocess.run(['python', 'src/MicroMacro.py'], capture_output=True, text=True)
     assert result.returncode == 0
-    assert "MicroMacro_pyRunner: Successful execution" in result.stdout
