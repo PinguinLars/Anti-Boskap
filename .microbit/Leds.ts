44e56ef88dcfaef8aaa70314644a41db0f07ddb0
PinguinLars1105 <Heijmen.Lars@gmail.com>
2025-01-15 13:56:43 +0100

    Adding the Micro:bit src files to this repo (WIP

---
+++ .microbit/Leds.ts
@@ -0,0 +1,21 @@
+
+radio.setGroup(115)
+let RedLed = false
+
+radio.onReceivedString(function(receivedString: string) {
+    if (receivedString = "ALARM") {
+        RedLed = true
+    } else if (receivedString = "no alarm") {
+        RedLed = false
+    }
+})
+
+basic.forever(function() {
+    if (RedLed = true) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    } else if (RedLed = false) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+})
