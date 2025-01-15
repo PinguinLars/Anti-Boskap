9b74e15f907e4a3a2dc20ad40fe2011f754a8252
PinguinLars1105 <Heijmen.Lars@gmail.com>
2025-01-15 15:27:49 +0100

    Big changes

--- .microbit/Alarm.ts
+++ .microbit/Alarm.ts
@@ -1,13 +1,13 @@
 
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground)
-radio.setGroup(115)
-radio.sendString("ALARM")
-let Alarm = true
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground);
+radio.setGroup(115);
+radio.sendString("ALARM");
+let Alarm = true;
 
 input.onButtonPressed(Button.A, function() {
-    if(Alarm = true) {
-        music.stopMelody(MelodyStopOptions.All)
-        Alarm = false
-        radio.sendString("no alarm")
+    if (Alarm) {
+        music.stopMelody(MelodyStopOptions.All);
+        Alarm = false;
+        radio.sendString("no alarm");
     }
-})
+});
