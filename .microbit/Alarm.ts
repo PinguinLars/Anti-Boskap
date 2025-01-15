871d8bf839c8167af643f05ad35c77213fcc2fd5
Lars <68437296+PinguinLars@users.noreply.github.com>
2025-01-15 15:38:05 +0100

    Revert "Revert "Adding the Micro:bit src files to this repo (WIP)""

---
+++ .microbit/Alarm.ts
@@ -0,0 +1,13 @@
+
+music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground);
+radio.setGroup(115);
+radio.sendString("ALARM");
+let Alarm = true;
+
+input.onButtonPressed(Button.A, function() {
+    if (Alarm) {
+        music.stopMelody(MelodyStopOptions.All);
+        Alarm = false;
+        radio.sendString("no alarm");
+    }
+});
