d185da0e337bfa82ff15292307d622667a86c50b
PinguinLarsAlt <PinguinLars1105@gmail.com>
2025-01-16 17:42:24 +0000

    Update Alarm.ts with latest main.ts from MicroAlarmSound

--- .microbit/Alarm.ts
+++ .microbit/Alarm.ts
@@ -3,7 +3,7 @@ radio.setGroup(115)
 radio.sendString("ALARM")
 let Alarm = true
 input.onButtonPressed(Button.A, function() {
-    if(Alarm == true) {
+    if(Alarm = true) {
         music.stopMelody(MelodyStopOptions.All)
         Alarm = false
         radio.sendString("no alarm")
