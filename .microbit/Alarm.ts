6ca125ead76bf2d171ef72d3bba20413e26741b8
PinguinLars1105 <Heijmen.Lars@gmail.com>
2025-01-23 13:07:52 +0100

    ALot of things

--- .microbit/Alarm.ts
+++ .microbit/Alarm.ts
@@ -1,10 +1,15 @@
-music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground)
+let Alarm = false
+music.setBuiltInSpeakerEnabled(false)
+// music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.LoopingInBackground)
+music.play(music.stringPlayable("C5 B C5 B C5 B C5 B ", 150), music.PlaybackMode.LoopingInBackground)
 radio.setGroup(115)
 radio.sendString("ALARM")
-let Alarm = true
-input.onButtonPressed(Button.A, function() {
-    if(Alarm == true) {
+Alarm = true
+
+input.onButtonPressed(Button.A, function () {
+    if (Alarm == true) {
         music.stopMelody(MelodyStopOptions.All)
+        music.stopAllSounds()
         Alarm = false
         radio.sendString("no alarm")
     }
--- .microbit/Leds.ts
+++ .microbit/Leds.ts
@@ -2,19 +2,19 @@ radio.setGroup(115)
 let RedLed = false
 
 radio.onReceivedString(function(receivedString: string) {
-    if (receivedString == "ALARM") {
+    if (receivedString === "ALARM") {
         RedLed = true
-    } else if (receivedString == "no alarm") {
+    } else if (receivedString === "no alarm") {
         RedLed = false
     }
 })
 
 basic.forever(function() {
-    if (RedLed == true) {
+    if (RedLed === true) {
         pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-    } else if (RedLed == false) {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else if (RedLed === false) {
         pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
     }
 })
