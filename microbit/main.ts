eaa2c41c408e257f15112c558736953ede76cdee
PinguinLars1105 <Heijmen.Lars@gmail.com>
2025-01-24 11:29:08 +0100

    Add remote deactivation

--- microbit/main.ts
+++ microbit/main.ts
@@ -1,3 +1,4 @@
+radio.setGroup(1)
 let Alarm = false
 music.setBuiltInSpeakerEnabled(false)
 music.play(music.stringPlayable("C5 B C5 B C5 B C5 B ", 150), music.PlaybackMode.LoopingInBackground)
@@ -5,6 +6,15 @@ Alarm = true
 pins.digitalWritePin(DigitalPin.P1, 1)
 
 input.onButtonPressed(Button.A, function () {
+    deactiveAlarm()
+    radio.sendString("Made by PinguinLars")
+})
+
+radio.onReceivedString(function(receivedString: string) {
+    deactiveAlarm()
+})
+
+function deactiveAlarm() {
     if (Alarm == true) {
         music.stopMelody(MelodyStopOptions.All)
         music.stopAllSounds()
@@ -12,4 +22,4 @@ input.onButtonPressed(Button.A, function () {
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
     }
-})
+}
